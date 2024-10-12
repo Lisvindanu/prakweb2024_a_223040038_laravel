58812b240a13273025467b55020c62d9a4e5ddaf
Lisvindanu <Lisvindanu015@gmail.com>
2024-10-13 00:56:22 +0700

    mengerjakan video 4 laravel 11

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+<a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/blog">blog</a>
+<a href="/contact">contact</a>
+<h1>Contact Me </h1>
+<ul>
+    <li>
+        Instagram : <a href="https://instagram.com/Lisvindanu">Instagram</a>
+
+    </li>
+</ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Lisvindanu']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+// Buat 2 rute baru
+// 1. /blog
+// 2. buat artikel, judul dan isi
+// 2. /contact
+// email dan sosmed
